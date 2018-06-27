df007c0ea11ebcf9ee26679386b7c76c9e9d4f18
Mathias Schilling <m@matchilling.com>
2018-06-27 11:48:55 +0200

    Fix linting

--- src/n00bify/index.js
+++ src/n00bify/index.js
@@ -8,34 +8,34 @@ export default function n00bify(text) {
 
   const punctuationPattern = /\.|,|'/g,
         replaceables = new Map()
-            .set('too', '2')
-            .set('Too', '2')
-            .set('to', '2')
-            .set('To', '2')
-            .set('fore', '4')
-            .set('Fore', '4')
-            .set('for', '4')
-            .set('For', '4')
-            .set('oo', '00')
-            .set('OO', '00')
-            .set('be', 'b')
-            .set('Be', 'b')
-            .set('are', 'r')
-            .set('Are', 'r')
-            .set('you', 'u')
-            .set('You', 'u')
-            .set('please', 'plz')
-            .set('Please', 'plz')
-            .set('people', 'ppl')
-            .set('People', 'ppl')
-            .set('really', 'rly')
-            .set('Really', 'rly')
-            .set('have', 'haz')
-            .set('Have', 'haz')
-            .set('know', 'no')
-            .set('Know', 'no')
-            .set('s', 'z')
-            .set('S', 'Z'),
+          .set('too', '2')
+          .set('Too', '2')
+          .set('to', '2')
+          .set('To', '2')
+          .set('fore', '4')
+          .set('Fore', '4')
+          .set('for', '4')
+          .set('For', '4')
+          .set('oo', '00')
+          .set('OO', '00')
+          .set('be', 'b')
+          .set('Be', 'b')
+          .set('are', 'r')
+          .set('Are', 'r')
+          .set('you', 'u')
+          .set('You', 'u')
+          .set('please', 'plz')
+          .set('Please', 'plz')
+          .set('people', 'ppl')
+          .set('People', 'ppl')
+          .set('really', 'rly')
+          .set('Really', 'rly')
+          .set('have', 'haz')
+          .set('Have', 'haz')
+          .set('know', 'no')
+          .set('Know', 'no')
+          .set('s', 'z')
+          .set('S', 'Z'),
         repeatSeq = (n, seq, acc) => {
           const val = seq.shift();
           seq.push(val);
@@ -48,10 +48,10 @@ export default function n00bify(text) {
         };
 
   let res = text
-      .split(' ')
-      .reduce((acc, token) => `${acc} ${transform(token)}`, '')
-      .replace(punctuationPattern, '')
-      .trim();
+    .split(' ')
+    .reduce((acc, token) => `${acc} ${transform(token)}`, '')
+    .replace(punctuationPattern, '')
+    .trim();
 
   if (0 === res.indexOf('w') || 0 === res.indexOf('W')) res = `LOL ${res}`;
   if (0 === res.indexOf('h') || 0 === res.indexOf('H')) res = res.toUpperCase();
@@ -60,6 +60,6 @@ export default function n00bify(text) {
   if (0 < res.indexOf('!')) res = res.replace(/!/g, repeatSeq(res.split(' ').length, ['!', '1'], ''));
 
   return res.split(' ')
-      .reduce((acc, token, index) => `${acc} ${0 !== index % 2 ? token.toUpperCase() : token}`, '')
-      .trim();
+    .reduce((acc, token, index) => `${acc} ${0 !== index % 2 ? token.toUpperCase() : token}`, '')
+    .trim();
 }
